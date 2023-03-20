9302a63673e7578eb3060c99ee262abcc73a9691
Ismail Chbiki <is.chbiki@gmail.com>
2023-03-20 17:29:07 +0100

    desktop image on banner removed and title added

--- src/Components/Header/Index.js
+++ src/Components/Header/Index.js
@@ -1,34 +1,13 @@
 import { useRef } from "react";
 
 // Import Custome Hooks
-import useAxios from "./../../CustomeHooks/useAxios/useAxios";
-import { useDarkTheme } from "./../../CustomeHooks/useDarkTheme/useDarkTheme";
 import useThrottle from "./../../CustomeHooks/useThrottle/useThrottle";
-
-// Import Components
-import Loading from "./../Loading/Index";
-import Error from "./../Error/Index";
 
 // Main Header Sass File
 import "./Index.scss";
 
 const Header = () => {
-  const darkTheme = useDarkTheme();
   const { throttle } = useThrottle();
-
-  // Fetch data
-  const {
-    data: {
-      title = { before: "", word: "", after: "" },
-      body = "",
-      button = "",
-      image = "",
-      image_dark = "",
-    },
-    success,
-    isPending,
-    error,
-  } = useAxios("./Apis/header.json", []);
 
   // Scroll To Services Section Ref
   const header = useRef();
@@ -43,33 +22,20 @@ const Header = () => {
 
   return (
     <header className="main-header" ref={header}>
-      {isPending && <Loading />}
-
       <div className="container">
-        {success && (
-          <div className="header-inner">
-            <section className="header-desc">
-              <h1 className="header-title">
-                {title.before} <span>{title.word}</span> {title.after}
-              </h1>
-              <p className="header-paragraph">{body}</p>
-              <button className="header-btn" onClick={exploreFn}>
-                {button}
-              </button>
-            </section>
-
-            <section className="header-image">
-              <img
-                src={darkTheme ? image_dark : image}
-                alt="header-Illustration"
-                draggable="false"
-              />
-            </section>
-          </div>
-        )}
+        <div className="header-inner">
+          <section className="header-desc">
+            <h1 className="header-title">
+              Unleash Your Adventure Spirit with Kitesurfing in
+              <span> Morocco</span>
+            </h1>
+            <p className="header-paragraph"></p>
+            <button className="header-btn" onClick={exploreFn}>
+              Explore
+            </button>
+          </section>
+        </div>
       </div>
-
-      {error && <Error message={error.message} name="Header" />}
     </header>
   );
 };
